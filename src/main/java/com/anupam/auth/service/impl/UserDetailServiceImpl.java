140d8f6abaa07609cc21939b4e8b2a9c816d56d9
anupamdas1511 <dasanupam151101@gmail.com>
2024-07-17 12:37:00 +0530

    UPDATE: Added notes

--- src/main/java/com/anupam/auth/service/impl/UserDetailServiceImpl.java
+++ src/main/java/com/anupam/auth/service/impl/UserDetailServiceImpl.java
@@ -1,14 +1,11 @@
 package com.anupam.auth.service.impl;
 
-import com.anupam.auth.entities.User;
 import com.anupam.auth.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
